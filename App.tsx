50e2e69406cf5f2e339467d51c0fcbc86f2089bb
FURIUM <shouldBnewB@gmail.com>
2025-10-22 13:25:23 +0900

    (add) WebView

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, SafeAreaView, StatusBar } from 'react-native';
+import WebView from 'react-native-webview';
+
+const styles = StyleSheet.create({
+  safeArea: { flex: 1, paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0 },
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <WebView source={{ uri: 'http://localhost:3000' }} />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
